754ffa12baeae91ebf40e1385c459e8e838ce568
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-01-31 04:36:29 +0100

    Add more testdata

--- src/util/UserLoginTestData.js
+++ src/util/UserLoginTestData.js
@@ -33,3 +33,72 @@ export const UserHubData = {
 		},
 	],
 };
+
+export const ListData = {
+	shoppingListId: "1",
+	name: "Första listan",
+	collaborator: {
+		username: "Lekkit",
+	},
+	owner: {
+		username: "luliin",
+	},
+	items: [
+		{
+			itemId: "1",
+			name: "Honung",
+			quantity: 1,
+			unit: "ST",
+			isCompleted: false,
+		},
+		{
+			itemId: "2",
+			name: "Ägg",
+			quantity: 24,
+			unit: "ST",
+			isCompleted: false,
+		},
+		{
+			itemId: "3",
+			name: "Senap",
+			quantity: 1,
+			unit: "ST",
+			isCompleted: false,
+		},
+		{
+			itemId: "4",
+			name: "Ketchup",
+			quantity: 1,
+			unit: "ST",
+			isCompleted: false,
+		},
+		{
+			itemId: "5",
+			name: "Avocado",
+			quantity: 5,
+			unit: "ST",
+			isCompleted: false,
+		},
+		{
+			itemId: "6",
+			name: "Babyspenat",
+			quantity: 2,
+			unit: "ST",
+			isCompleted: false,
+		},
+		{
+			itemId: "7",
+			name: "Duschkräm",
+			quantity: 1,
+			unit: "ST",
+			isCompleted: false,
+		},
+		{
+			itemId: "8",
+			name: "Mjölk",
+			quantity: 3,
+			unit: "L",
+			isCompleted: true,
+		},
+	],
+};
