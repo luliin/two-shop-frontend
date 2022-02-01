5ab0903a33ee39c8a1a4c3e5018c511e3960b606
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-02-01 10:08:56 +0100

    Add custom select styles

--- src/components/input/FormInputStyles.jsx
+++ src/components/input/FormInputStyles.jsx
@@ -14,7 +14,23 @@ export const InputField = styled.input.attrs(({ type }) => ({
 	font-size: 1em;
 `;
 
+export const Select = styled.select.attrs(({ value }) => ({
+	value: value || "",
+}))`
+	margin: 1em 0;
+	width: ${({ width }) => (width ? width : "min(250px, 56vw)")};
+	border-radius: 2em;
+	border: none;
+	height: 4vh;
+	padding: 0 13px;
+	font-family: "Barlow Condensed", sans-serfif;
+	font-size: 1em;
 
-export const LabelStyled = styled.label`
-	
+	optgroup,
+	option {
+		color: ${({ theme }) => theme.colors.lightishText};
+		background: ${({ theme }) => theme.colors.lighterBackground};
+	}
 `;
+
+export const LabelStyled = styled.label``;
