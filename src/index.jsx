ee54f5832547f81d22fe6dbf57be41ea9bf795d1
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-02-02 23:47:02 +0100

    Wrap app in client and inject it into App

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,16 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./components/app/AppStyles";
+import { ApolloProvider } from "@apollo/client";
+import client from "./common/apollo-client";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<ThemeProvider theme={Theme}>
 			<Router>
-				<App />
+				<ApolloProvider client={client}>
+					<App client={client} />
+				</ApolloProvider>
 			</Router>
 		</ThemeProvider>
 	</React.StrictMode>,
