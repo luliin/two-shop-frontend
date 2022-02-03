c37cbe4a65825d532089b6568ae8b6ef24c1a9d5
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-02-03 02:46:14 +0100

    Add backend integration to hub page

--- src/pages/hub/ShoppingListHubPage.jsx
+++ src/pages/hub/ShoppingListHubPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ShoppingListCard from "../../components/cards/ShoppingListCard";
 import { buttonVariants } from "../../util/AnimationVariants";
 import {
@@ -31,14 +32,19 @@ import { WideButtonStyled } from "../../components/buttons/WideButtonStyled";
 import { UserHubData } from "../../util/UserLoginTestData";
 import { PlusIcon } from "../list/ListStyles";
 import { IconWrapper } from "../../components/icons/IconWrapper";
+import { UserContext } from "../../context/UserContext";
+import { useGetUserShoppingLists } from "../../hooks/shoppingLists/useGetShoppingLists";
+import authService from "../../services/auth.service";
 
 const hubData = UserHubData;
 
 const ShoppingListHubPage = () => {
+	const navigate = useNavigate();
 	const [listName, setListName] = useState("");
 	const [collaboratorCredential, setCollaboratorCredential] = useState("");
 	const [show, setShow] = useState(false);
 	const [hover, setHover] = useState(false);
+	const { user } = useContext(UserContext);
 
 	const handleShowModal = () => {
 		setShow(!show);
@@ -53,158 +59,186 @@ const ShoppingListHubPage = () => {
 		console.log(createShoppingListInput);
 	};
 
+	useEffect(() => {
+		if (!user) {
+			navigate({ to: "/" });
+		}
+	}, [navigate, user]);
+	let data = useGetUserShoppingLists(authService.getCurrentUser().id);
 	return (
 		<>
-			<TopBar>
-				<IconWithLabel>
-					{<OwnerIcon pt={""} />} = Ägs av dig
-				</IconWithLabel>
-				<IconWithLabel>
-					{<CollaboratorIcon pt={""} />} = Delas med dig
-				</IconWithLabel>
-			</TopBar>
-			<OuterContainer>
-				<HubContainer>
-					{hubData.ownedShoppingLists.map((list) => (
-						<ShoppingListCard
-							{...{
-								id: list.id,
-								isOwner: true,
-								collaborator: list.collaborator.username,
-								name: list.name,
-								owner: hubData.username,
-							}}
-							key={list.id}
-						/>
-					))}
-					{hubData.collaboratorShoppingLists.map((list) => (
-						<ShoppingListCard
-							{...{
-								id: list.id,
-								isOwner: false,
-								owner: list.owner.username,
-								name: list.name,
-								collaborator: hubData.username,
-							}}
-							key={list.id}
-						/>
-					))}
+			{user && (
+				<>
+					<TopBar>
+						<IconWithLabel>
+							{<OwnerIcon pt={""} />} = Ägs av dig
+						</IconWithLabel>
+						<IconWithLabel>
+							{<CollaboratorIcon pt={""} />} = Delas med dig
+						</IconWithLabel>
+					</TopBar>
+					<OuterContainer>
+						{data && (
+							<HubContainer>
+								{data.ownedShoppingLists.map((list) => (
+									<ShoppingListCard
+										{...{
+											id: list.id,
+											isOwner: true,
+											collaborator: list.collaborator
+												? list.collaborator.username
+												: null,
+											name: list.name,
+											owner: data.username,
+										}}
+										key={list.id}
+									/>
+								))}
+								{data.collaboratorShoppingLists.map((list) => (
+									<ShoppingListCard
+										{...{
+											id: list.id,
+											isOwner: false,
+											owner: list.owner
+												? list.owner.username
+												: null,
+											name: list.name,
+											collaborator: data.username,
+										}}
+										key={list.id}
+									/>
+								))}
 
-					<RoundButton
-						onClick={handleShowModal}
-						variants={buttonVariants}
-						whileHover={buttonVariants.hover}
-						type="button"
-						title="Skapa ny lista"
-					>
-						<IconWrapper>
-							<PlusIcon />
-						</IconWrapper>
-					</RoundButton>
-				</HubContainer>
-				<AnimatePresence>
-					{show && (
-						<Model
-							exit={{ opacity: 0 }}
-							key="backdrop"
-							transition={{ duration: 0.2 }}
-							show={"show"}
-							onClick={() => {
-								if (!hover) {
-									setShow(false);
-								}
-							}}
-						>
-							<ModalContainer
-								exit={{ y: "-200vh" }}
-								key="modal"
-								initial={{
-									opacity: 0,
-									x: "-50%",
-									y: "-50%",
-								}}
-								animate={{
-									opacity: 1,
-									x: "-50%",
-									y: "-50%",
-								}}
-								onMouseEnter={() => {
-									setHover(true);
-								}}
-								onMouseLeave={() => {
-									setHover(false);
-								}}
-							>
-								<FormHeading>Skapa ny lista</FormHeading>
-								<FormStyled onSubmit={handleCreateShoppingList}>
-									<InputRow>
-										<LabelStyled htmlFor="listname">
-											<ListIcon title="Ange namnet du vill ge den nya shoppinglistan" />
-										</LabelStyled>
-										<InputField
-											id="new-list-name"
-											width={"min(250px, 56vw)"}
-											autoComplete="off"
-											type="text"
-											placeholder={"Listans namn"}
-											name="listname"
-											required
-											onChange={(e) =>
-												setListName(e.target.value)
-											}
-										/>
-									</InputRow>
-									<InputRow>
-										Vill du samarbeta med någon?
-									</InputRow>
-									<InputRow>
-										Ange e-post eller användarnamn till hen
-									</InputRow>
-									<InputRow>
-										<LabelStyled htmlFor="collaborator">
-											<CollaboratorIcon
-												title="Användarnamn eller e-post till kollaboratör"
-												fs={"24px"}
-												color={"light"}
-											/>
-										</LabelStyled>
-										<InputField
-											width={"min(250px, 56vw)"}
-											autoComplete="off"
-											type="text"
-											placeholder={
-												"Användarnamn eller e-post"
-											}
-											name="collaborator"
-											onChange={(e) =>
-												setCollaboratorCredential(
-													e.target.value
-												)
-											}
-										/>
-									</InputRow>
-									<WideButtonStyled
-										width={"min(300px, 67vw)"}
-										whileHover={buttonVariants.hover}
-										whileTap={buttonVariants.tapGlow}
+								<RoundButton
+									onClick={handleShowModal}
+									variants={buttonVariants}
+									whileHover={buttonVariants.hover}
+									type="button"
+									title="Skapa ny lista"
+								>
+									<IconWrapper>
+										<PlusIcon />
+									</IconWrapper>
+								</RoundButton>
+							</HubContainer>
+						)}
+
+						<AnimatePresence>
+							{show && (
+								<Model
+									exit={{ opacity: 0 }}
+									key="backdrop"
+									transition={{ duration: 0.2 }}
+									show={"show"}
+									onClick={() => {
+										if (!hover) {
+											setShow(false);
+										}
+									}}
+								>
+									<ModalContainer
+										exit={{ y: "-200vh" }}
+										key="modal"
+										initial={{
+											opacity: 0,
+											x: "-50%",
+											y: "-50%",
+										}}
+										animate={{
+											opacity: 1,
+											x: "-50%",
+											y: "-50%",
+										}}
+										onMouseEnter={() => {
+											setHover(true);
+										}}
+										onMouseLeave={() => {
+											setHover(false);
+										}}
 									>
-										{"Bekräfta"}
-									</WideButtonStyled>
-								</FormStyled>
-								<FormDivider
-									mt={"20px"}
-									mb={"20px"}
-									width={"min(300px, 67vw)"}
-								/>
-								<InputRow>
-									Du kan även bjuda in någon till listan
-									senare!
-								</InputRow>
-							</ModalContainer>
-						</Model>
-					)}
-				</AnimatePresence>
-			</OuterContainer>
+										<FormHeading>
+											Skapa ny lista
+										</FormHeading>
+										<FormStyled
+											onSubmit={handleCreateShoppingList}
+										>
+											<InputRow>
+												<LabelStyled htmlFor="listname">
+													<ListIcon title="Ange namnet du vill ge den nya shoppinglistan" />
+												</LabelStyled>
+												<InputField
+													id="new-list-name"
+													width={"min(250px, 56vw)"}
+													autoComplete="off"
+													type="text"
+													placeholder={"Listans namn"}
+													name="listname"
+													required
+													onChange={(e) =>
+														setListName(
+															e.target.value
+														)
+													}
+												/>
+											</InputRow>
+											<InputRow>
+												Vill du samarbeta med någon?
+											</InputRow>
+											<InputRow>
+												Ange e-post eller användarnamn
+												till hen
+											</InputRow>
+											<InputRow>
+												<LabelStyled htmlFor="collaborator">
+													<CollaboratorIcon
+														title="Användarnamn eller e-post till kollaboratör"
+														fs={"24px"}
+														color={"light"}
+													/>
+												</LabelStyled>
+												<InputField
+													width={"min(250px, 56vw)"}
+													autoComplete="off"
+													type="text"
+													placeholder={
+														"Användarnamn eller e-post"
+													}
+													name="collaborator"
+													onChange={(e) =>
+														setCollaboratorCredential(
+															e.target.value
+														)
+													}
+												/>
+											</InputRow>
+											<WideButtonStyled
+												width={"min(300px, 67vw)"}
+												whileHover={
+													buttonVariants.hover
+												}
+												whileTap={
+													buttonVariants.tapGlow
+												}
+											>
+												{"Bekräfta"}
+											</WideButtonStyled>
+										</FormStyled>
+										<FormDivider
+											mt={"20px"}
+											mb={"20px"}
+											width={"min(300px, 67vw)"}
+										/>
+										<InputRow>
+											Du kan även bjuda in någon till
+											listan senare!
+										</InputRow>
+									</ModalContainer>
+								</Model>
+							)}
+						</AnimatePresence>
+					</OuterContainer>
+				</>
+			)}
 		</>
 	);
 };
