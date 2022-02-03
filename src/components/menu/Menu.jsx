055e6141de407f0229a57811818a76010aac4e86
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-02-03 03:13:29 +0100

    Add dynamic menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,15 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 import { ListItem, MenuContainer, MenuList, TextWrapper } from "./MenuStyles";
+import logout from "../../services/auth.service";
 
-const Menu = ({shouldClose}) => {
-
-	const [close, setClose] = shouldClose
-	const variants = {
-		hover: {
-			scale: 1.1,
-		},
-	};
+const Menu = ({ shouldClose }) => {
+	const [close, setClose] = shouldClose;
+	const { user } = useContext(UserContext);
 	return (
 		<MenuContainer
 			initial={{ x: 500, y: 0, opacity: 0 }}
@@ -27,11 +24,9 @@ const Menu = ({shouldClose}) => {
 			}}
 		>
 			<MenuList>
-				<li>
-					<ListItem>
-						<Link
-							to={"/login"}
-						>
+				{user ? (
+					<>
+						<li>
 							<TextWrapper
 								whileHover={{
 									borderRadius: "2em",
@@ -41,45 +36,72 @@ const Menu = ({shouldClose}) => {
 									textShadow: "0 0 8px rgb(231,175,61)",
 									letterSpacing: "2px",
 								}}
-								onClickCapture={()=> {
+								onClickCapture={() => {
 									setClose(true);
+									logout();
 								}}
 							>
-								Logga in
+								Logga ut
 							</TextWrapper>
-						</Link>
-					</ListItem>
-				</li>
-				<li>
-					<ListItem>
-						<TextWrapper
-							whileHover={{
-								borderRadius: "2em",
-								boxShadow: "0 0 8px rgb(231, 175, 61)",
-								scale: 1.1,
-								textShadow: "0 0 8px rgb(231,175,61)",
-								letterSpacing: "2px",
-							}}
-						>
-							Mina shoppinglistor
-						</TextWrapper>
-					</ListItem>
-				</li>
-				<li>
-					<ListItem>
-						<TextWrapper
-							whileHover={{
-								borderRadius: "2em",
-								boxShadow: "0 0 8px rgb(231, 175, 61)",
-								scale: 1.1,
-								textShadow: "0 0 8px rgb(231,175,61)",
-								letterSpacing: "2px",
-							}}
-						>
-							Inställningar
-						</TextWrapper>
-					</ListItem>
-				</li>
+						</li>
+						<li>
+							<ListItem>
+								<TextWrapper
+									whileHover={{
+										borderRadius: "2em",
+										boxShadow: "0 0 8px rgb(231, 175, 61)",
+										scale: 1.1,
+										textShadow: "0 0 8px rgb(231,175,61)",
+										letterSpacing: "2px",
+									}}
+								>
+									Mina shoppinglistor
+								</TextWrapper>
+							</ListItem>
+						</li>
+						<li>
+							<ListItem>
+								<TextWrapper
+									whileHover={{
+										borderRadius: "2em",
+										boxShadow: "0 0 8px rgb(231, 175, 61)",
+										scale: 1.1,
+										textShadow: "0 0 8px rgb(231,175,61)",
+										letterSpacing: "2px",
+									}}
+								>
+									Inställningar
+								</TextWrapper>
+							</ListItem>
+						</li>
+					</>
+				) : (
+					<>
+						<li>
+							<ListItem>
+								<Link to={"/login"}>
+									<TextWrapper
+										whileHover={{
+											borderRadius: "2em",
+											boxShadow:
+												"0 0 8px rgb(231, 175, 61)",
+
+											scale: 1.1,
+											textShadow:
+												"0 0 8px rgb(231,175,61)",
+											letterSpacing: "2px",
+										}}
+										onClickCapture={() => {
+											setClose(true);
+										}}
+									>
+										Logga in
+									</TextWrapper>
+								</Link>
+							</ListItem>
+						</li>
+					</>
+				)}
 			</MenuList>
 		</MenuContainer>
 	);
