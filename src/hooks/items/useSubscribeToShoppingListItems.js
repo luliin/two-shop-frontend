7968ae990e79a7c750f0ac0697a5ec7ff8bfb76f
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-02-04 05:02:44 +0100

    Add subscription hook

---
+++ src/hooks/items/useSubscribeToShoppingListItems.js
@@ -0,0 +1,23 @@
+import { gql, useSubscription } from "@apollo/client";
+
+const ITEM_MODIFIED = gql`
+	subscription ItemModified($shoppingListId: ID!) {
+		itemModified(shoppingListId: $shoppingListId) {
+			items {
+				id
+				name
+				quantity
+				unit
+				isCompleted
+			}
+		}
+	}
+`;
+
+export const useSubscribeToItemModified = (shoppingListId, data) => {
+	const itemModified = useSubscription(ITEM_MODIFIED, {
+		variables: { shoppingListId: shoppingListId },
+	});
+
+	return itemModified;
+};
